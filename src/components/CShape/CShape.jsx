8d7049757c852a2458c91f770de19059a0ce57d7
sampath surineni <sampath.surineni@gmail.com>
2026-05-25 00:30:46 +0530

    Added cshapre problem

---
+++ src/components/CShape/CShape.jsx
@@ -0,0 +1,2 @@
+// https://medium.com/womenintechnology/commonly-asked-machine-coding-problem-in-front-end-interviews-e16042ab1e10
+// https://www.youtube.com/watch?v=sAF7Km_znjA
--- src/components/TempPlayground.jsx
+++ src/components/TempPlayground.jsx
@@ -1,16 +1,56 @@
-import styles from "./TempPlayground.module.css"
-export default function TempPlayground() {
-  return (<>
-    <ProgressBar fillPercentage={50} />
-  </>)
-}
+import {useState} from 'react'
+import styles from './TempPlayground.module.css'
+const ROWS = 6;
+const COLS = 7;
 
-function ProgressBar({ fillPercentage }) {
-  return (
-    <div className={styles["progress-bar"]}>
-      <div className={styles["progress-fill"]} style={{
-        width: `${fillPercentage}%`
-      }}>{fillPercentage}%</div>
-    </div>
-  )
+export default function Connect4() {
+  const [board, setBoard] = useState(Array(ROWS).fill(null).map(() => Array(COLS).fill(null)))
+  const [winner, setWinner] = useState(false);
+  const [redTurn, isRedTurn] = useState(true)
+  console.log('board', board)
+  const currentPlayer = isRedTurn? 'Red': 'Blue'
+
+  const checkBoardBounds = (rowIdx, colIdx) => {
+    if (rowIdx >= 0 && rowIdx < ROWS && colIdx >= 0 && colIdx < COLS) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  const checkWin = (board, rowIdx, colIdx, currentPlayer) => {
+    const directions = [[0, 1], [1, 0], [1, 1], [1, -1]];
+    let matches = 0;
+    for (const [dr, dc] of directions) {
+      if (checkBoardBounds(rowIdx + (dr * (matches + 1)), colIdx + (dc * (matches + 1))) && board[rowIdx][colIdx] == currentPlayer) {
+        matches += 1;
+      }
+    }
+
+  }
+  const handleColumnClick = (colIdx) => {
+    for (let rowIdx = ROWS - 1; rowIdx >= 0; rowIdx--) {
+      if (board[rowIdx][colIdx] === null) {
+        const newBoard = board.map(row => [...row])
+        newBoard[rowIdx][colIdx] = currentPlayer;
+        setBoard(newBoard)
+        if (checkWin(newBoard, rowIdx, colIdx, currentPlayer)) {
+          setWinner(currentPlayer)
+        } else {
+          setIsRedTurn(!isRedTurn)
+        }
+        return
+      }
+    }
+
+  }
+  return (<div className={styles.board}>
+    {board.map((row, rowIdx) => (
+      row.map((colVal, colIdx) => (
+        <div key={`${rowIdx}-${colIdx}`}
+        className={styles.cell}
+        onClick={()=> handleColumnClick(colIdx)}>
+          </div>
+      ))
+    ))}
+  </div>)
 }
