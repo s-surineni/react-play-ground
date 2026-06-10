ad6f3f5b293aa092188d82b39130f5985689c43c
Sampath Surineni <sampath.surineni@gmail.com>
2026-06-10 06:56:16 +0530

    Added file structure for components and utils folders.

--- src/components/FileExplorer/FileExplorerNormalized.jsx
+++ src/components/FileExplorer/FileExplorerNormalized.jsx
@@ -5,12 +5,62 @@ const nodes = {
     id:1,
     name:"src",
     type:"folder",
-    children:[2]
+    children:[2,3]
   },
 
   2:{
     id:2,
     name:"App.jsx",
+    type:"file"
+  },
+
+  3: {
+    id:3,
+    name:"components",
+    type:"folder",
+    children:[4,5]
+  },
+
+  4: {
+    id:4,
+    name:"Button.jsx",
+    type:"file"
+  },
+
+  5: {
+    id:5,
+    name:"Header.jsx",
+    type:"file"
+  },
+
+  6: {
+    id:6,
+    name:"utils",
+    type:"folder",
+    children:[7,8,9]
+  },
+
+  7: {
+    id:7,
+    name:"helpers.js",
+    type:"file"
+  },
+
+  8: {
+    id:8,
+    name:"api.js",
+    type:"file"
+  },
+
+  9: {
+    id:9,
+    name:"constants.js",
+    type:"file"
+  },
+
+  10: {
+    id:10,
+    name:"index.js",
     type:"file"
   }
 }
@@ -46,9 +96,13 @@ function TreeNode({node}) {
   if (node.type == "folder") {
     return (<div onClick={() => setOpen(!open)}>
       📁 {node.name}
-      {open && node.children.map(child => (
-        chi
-      ))}
+{open && node.children && (
+        <div style={{ paddingLeft: "20px" }}>
+          {node.children.map(childId => (
+            <TreeNode key={childId} node={nodes[childId]} />
+          ))}
+        </div>
+      )}
     </div>
       
   )
