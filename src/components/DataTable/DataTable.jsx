cb6d70bdc3a1061e97383c39f3e736f0f3cb8a16
Sampath Surineni <sampath.surineni@gmail.com>
2026-08-13 23:56:15 +0530

    Auto-sync from ssurineni@ssurine-ltm3c4c.internal.salesforce.com

--- src/components/DataTable/DataTable.jsx
+++ src/components/DataTable/DataTable.jsx
@@ -1,6 +1,6 @@
 function DataTable({}) {
     return (<table>
-        <thead><th>ID</th><th>Name</th><th>Age</th><th>Occupation</th></thead>
+        <thead><tr><th>ID</th><th>Name</th><th>Age</th><th>Occupation</th></tr></thead>
         <tr><th>ID</th><th>Name</th><th>Age</th><th>Occupation</th></tr>
 
     </table>)
