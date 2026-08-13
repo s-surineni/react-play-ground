afa6cb7fbaf490de791d9fe23a32b084af8ab5b2
Sampath Surineni <sampath.surineni@gmail.com>
2026-08-14 04:30:24 +0530

    Auto-sync from ssurineni@ssurine-ltm3c4c.internal.salesforce.com

--- src/components/DataTable/DataTable.jsx
+++ src/components/DataTable/DataTable.jsx
@@ -1,8 +1,12 @@
-function DataTable({}) {
+import data from './data.js'
+function DataTable({data =[]}) {
     return (<table>
         <caption>People</caption>
         <thead><tr><th>ID</th><th>Name</th><th>Age</th><th>Occupation</th></tr></thead>
-        <tr><th>ID</th><th>Name</th><th>Age</th><th>Occupation</th></tr>
+        <tbody>
+           { data.map( row => <tr><td>ID</td><td>ID</td><td>ID</td><td>ID</td></tr>)}
+            
+        </tbody>
 
     </table>)
 }
