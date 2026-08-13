2a6054e5dbf59412892241d69dd8cdfc82d7efd0
Sampath Surineni <sampath.surineni@gmail.com>
2026-08-13 20:20:24 +0530

    Auto-sync from ssurineni@ssurine-ltm3c4c.internal.salesforce.com

---
+++ src/components/DataTable/DataTable.jsx
@@ -0,0 +1,9 @@
+function DataTable({}) {
+    return (<table>
+        <thead><th>ID</th><th>Name</th><th>Age</th><th>Occupation</th></thead>
+        <tr><th>ID</th><th>Name</th><th>Age</th><th>Occupation</th></tr>
+
+    </table>)
+}
+
+export default DataTable
