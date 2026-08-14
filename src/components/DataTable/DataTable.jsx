926733a192a1a8a98f259f407daee1cf454341f8
Sampath Surineni <sampath.surineni@gmail.com>
2026-08-14 06:00:24 +0530

    Auto-sync from ssurineni@ssurine-ltm3c4c.internal.salesforce.com

--- src/components/DataTable/DataTable.jsx
+++ src/components/DataTable/DataTable.jsx
@@ -1,17 +1,20 @@
 import data from './data.js'
 import { useState } from 'react'
 function DataTable() {
-    const [pageSizes, setPageSizes] = useState([5, 10, 15])
+    const pageSizes = [5, 10, 15]
+    const [pageSize, setPageSize] = useState(5)
+    const [pageNumber, setPageNumber] = useState(1)
+    const currentData = data.slice((pageNumber - 1) * pageSize, pageNumber * pageSize)
     return (
         <>
-            <select>
+            <select onChange={e => setPageSize(e.target.value)}>
                 {pageSizes.map(size => <option key={size} value={size}>{size}</option>)}
             </select>
             <table>
             <caption>People</caption>
             <thead><tr><th>ID</th><th>Name</th><th>Age</th><th>Occupation</th></tr></thead>
-            <tbody>wh
-                {data.map(row => <tr key={row['id']}><td>{row['id']}</td><td>{row['name']}</td><td>{row['age']}</td><td>{row['occupation']}</td></tr>)}
+            <tbody>
+                {currentData.map(row => <tr key={row['id']}><td>{row['id']}</td><td>{row['name']}</td><td>{row['age']}</td><td>{row['occupation']}</td></tr>)}
 
             </tbody>
 
