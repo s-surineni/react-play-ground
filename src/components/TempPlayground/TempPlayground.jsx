bbff02f00bdd826dd0d0928659bff4f700b5c249
Sampath Surineni <sampath.surineni@gmail.com>
2026-08-21 11:50:30 +0530

    Auto-sync from ssurineni@ssurine-ltm3c4c.internal.salesforce.com

--- src/components/TempPlayground/TempPlayground.jsx
+++ src/components/TempPlayground/TempPlayground.jsx
@@ -1,5 +1,15 @@
 import style from './TempPlayground.module.css'
+
+const CELLS = 6 * 6
+
 function TempPlayground() {
-  return (<div className={style.board}></div>)
+  return (
+    <div className={style.board}>
+      {Array.from({ length: CELLS }, (_, i) => (
+        <div key={i} className={style.cell} />
+      ))}
+    </div>
+  )
 }
+
 export default TempPlayground
