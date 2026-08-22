d55c1bb1de2692af651f7a1fa86061503ab2a4c5
Sampath Surineni <sampath.surineni@gmail.com>
2026-08-22 05:50:25 +0530

    Auto-sync from ssurineni@ssurine-ltm3c4c.internal.salesforce.com

--- src/components/TempPlayground/TempPlayground.jsx
+++ src/components/TempPlayground/TempPlayground.jsx
@@ -10,11 +10,24 @@ function TempPlayground() {
   )
   const [player, setPlayer] = useState('red')
 
+  const handleCellClick = (r, c) => {
+    if (board[r][c]) return
+
+    const nextBoard = board.map((row) => [...row])
+    nextBoard[r][c] = player
+    setBoard(nextBoard)
+    setPlayer(player === 'red' ? 'blue' : 'red')
+  }
+
   return (
     <div className={style.board}>
       {board.map((row, r) =>
         row.map((cell, c) => (
-          <div key={`${r}-${c}`} className={style.cell} />
+          <div
+            key={`${r}-${c}`}
+            className={`${style.cell} ${cell ? style[cell] : ''}`}
+            onClick={() => handleCellClick(r, c)}
+          />
         ))
       )}
     </div>
