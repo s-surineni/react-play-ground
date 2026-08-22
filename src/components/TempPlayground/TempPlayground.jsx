557db05657fa4457403d5f5520d939f107cc66ab
Sampath Surineni <sampath.surineni@gmail.com>
2026-08-22 16:50:26 +0530

    Auto-sync from ssurineni@ssurine-ltm3c4c.internal.salesforce.com

--- src/components/TempPlayground/TempPlayground.jsx
+++ src/components/TempPlayground/TempPlayground.jsx
@@ -11,12 +11,21 @@ function TempPlayground() {
   )
   const [player, setPlayer] = useState('red')
 
+  function findFreeRow(col) {
+    let currRow = board.length - 1
+    while(board[currRow][col] != null) {
+      currRow--;
+    }
+    return currRow;
+  }
+
   const handleCellClick = (r, c) => {
     if (board[r][c]) return
 
     // LEARN: copy the 2D array before writing. Mutating board[r][c] in place does not re-render.
     const nextBoard = board.map((row) => [...row])
-    nextBoard[r][c] = player
+    const rowToMark = findFreeColumn(c)
+    nextBoard[rowToMark][c] = player
     setBoard(nextBoard)
     setPlayer(player === 'red' ? 'blue' : 'red')
   }
