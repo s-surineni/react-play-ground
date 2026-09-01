d3ee662cc9820d4d3639755c6b8bee3b3b68998a
Sampath Surineni <sampath.surineni@gmail.com>
2026-09-01 15:50:26 +0530

    Auto-sync from ssurineni@ssurine-ltm3c4c.internal.salesforce.com

--- src/components/TempPlayground/TempPlayground.jsx
+++ src/components/TempPlayground/TempPlayground.jsx
@@ -66,96 +66,9 @@ const folderStructure = {
   }
 }
 
-function renameInTree(nodes, id, newName) {
-  return nodes.map((node) => {
-    if (node.id === id) {
-      return { ...node, name: newName }
-    }
-    if (node.children) {
-      return { ...node, children: renameInTree(node.children, id, newName) }
-    }
-    return node
-  })
-}
-
 function TempPlayground() {
-  const [files, setFiles] = useState(folderStructure)
-
-  const handleRename = (id, newName) => {
-    setFiles((prev) => renameInTree(prev, id, newName))
-  }
-
-  return <FileExplorer files={files} onRename={handleRename} />
-}
-
-function FileItem({ aFile, onRename }) {
-  const [open, setOpen] = useState(false)
-  const [isRenaming, setIsRenaming] = useState(false)
-  const [editValue, setEditValue] = useState(aFile.name)
-  const isFolder = Array.isArray(aFile.children)
-
-  const save = () => {
-    const next = editValue.trim()
-    if (next && next !== aFile.name) {
-      onRename(aFile.id, next)
-    } else {
-      setEditValue(aFile.name)
-    }
-    setIsRenaming(false)
-  }
-
-  return (
-    <div>
-      <span
-        onClick={(e) => {
-          e.stopPropagation()
-          if (isFolder && !isRenaming) {
-            setOpen((prev) => !prev)
-          }
-        }}
-      >
-        {isRenaming ? (
-          <input
-            autoFocus
-            value={editValue}
-            onChange={(e) => setEditValue(e.target.value)}
-            onBlur={save}
-            onClick={(e) => e.stopPropagation()}
-            onKeyDown={(e) => {
-              if (e.key === 'Enter') {
-                save()
-              }
-              if (e.key === 'Escape') {
-                setEditValue(aFile.name)
-                setIsRenaming(false)
-              }
-            }}
-          />
-        ) : (
-          <span
-            onDoubleClick={(e) => {
-              e.stopPropagation()
-              setEditValue(aFile.name)
-              setIsRenaming(true)
-            }}
-          >
-            {aFile.name}
-          </span>
-        )}
-      </span>
-      {isFolder && open && (
-        <div className={styles.children}>
-          <FileExplorer files={aFile.children} onRename={onRename} />
-        </div>
-      )}
-    </div>
-  )
-}
-
-function FileExplorer({ files, onRename }) {
-  return files.map((node) => (
-    <FileItem key={node.id} aFile={node} onRename={onRename} />
-  ))
+  const childIds = folderStructure.map((item) => console.log(item))
+  return 'hello'
 }
 
 export default TempPlayground
