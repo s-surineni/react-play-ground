0525cd4e690c7b562b5cab529f6b4fe31c24fa56
Sampath Surineni <sampath.surineni@gmail.com>
2026-09-01 15:31:50 +0530

    Auto-sync from ssurineni@ssurine-ltm3c4c.internal.salesforce.com

--- src/components/TempPlayground/TempPlayground.jsx
+++ src/components/TempPlayground/TempPlayground.jsx
@@ -1,50 +1,70 @@
 import { useState } from 'react'
 import styles from './TempPlayground.module.css'
 
-const folderStructure = [
-  {
+const folderStructure = {
+  1: {
     id: 1,
-    name: 'README.md',
+    name: "src",
+    type: "folder",
+    children: [2, 3, 6, 10]
   },
-  {
+
+  2: {
     id: 2,
-    name: 'Documents',
-    children: [
-      {
-        id: 3,
-        name: 'Word.doc',
-      },
-      {
-        id: 4,
-        name: 'Powerpoint.ppt',
-      },
-    ],
+    name: "App.jsx",
+    type: "file"
   },
-  {
+
+  3: {
+    id: 3,
+    name: "components",
+    type: "folder",
+    children: [4, 5]
+  },
+
+  4: {
+    id: 4,
+    name: "Button.jsx",
+    type: "file"
+  },
+
+  5: {
     id: 5,
-    name: 'Downloads',
-    children: [
-      {
-        id: 6,
-        name: 'unnamed.txt',
-      },
-      {
-        id: 7,
-        name: 'Misc',
-        children: [
-          {
-            id: 8,
-            name: 'foo.txt',
-          },
-          {
-            id: 9,
-            name: 'bar.txt',
-          },
-        ],
-      },
-    ],
+    name: "Header.jsx",
+    type: "file"
   },
-]
+
+  6: {
+    id: 6,
+    name: "utils",
+    type: "folder",
+    children: [7, 8, 9]
+  },
+
+  7: {
+    id: 7,
+    name: "helpers.js",
+    type: "file"
+  },
+
+  8: {
+    id: 8,
+    name: "api.js",
+    type: "file"
+  },
+
+  9: {
+    id: 9,
+    name: "constants.js",
+    type: "file"
+  },
+
+  10: {
+    id: 10,
+    name: "index.js",
+    type: "file"
+  }
+}
 
 function renameInTree(nodes, id, newName) {
   return nodes.map((node) => {
