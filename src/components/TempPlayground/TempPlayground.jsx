0a73b5e1d05b695f44fd982fa6559ead9cc4fe98
Sampath Surineni <sampath.surineni@gmail.com>
2026-09-10 12:10:28 +0530

    Auto-sync from ssurineni@ssurine-ltm3c4c.internal.salesforce.com

--- src/components/TempPlayground/TempPlayground.jsx
+++ src/components/TempPlayground/TempPlayground.jsx
@@ -4,6 +4,9 @@ function TempPlayground() {
   const pageSizes = [5, 10, 15]
   const [selectedPageSize, setSelectedPageSize] = useState(pageSizes[0])
   const [cursorPos, setCursorPos] = useState(0)
+  function sortByName() {
+    users.sort()
+  }
   return <>
     <label>
       Rows :
@@ -18,7 +21,7 @@ function TempPlayground() {
       <button onClick={() => setCursorPos(cursorPos - selectedPageSize)} disabled={cursorPos <= 0}>prev</button>
       <button onClick={() => setCursorPos(cursorPos + selectedPageSize)} disabled={cursorPos + selectedPageSize >= users.length}>next</button>
     <table>
-      <thead><tr><th>Name</th><th>Age</th><th>Occupation</th></tr></thead>
+      <thead><tr><th onClick={sortByName}>Name</th><th>Age</th><th>Occupation</th></tr></thead>
       <tbody>
         {users.slice(cursorPos, cursorPos+ selectedPageSize).map(aUser => <tr key={aUser.id}><td>{aUser.name}</td><td>{aUser.age}</td><td>{aUser.occupation}</td></tr>)}
       </tbody>
