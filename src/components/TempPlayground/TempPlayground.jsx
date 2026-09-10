2837cb527d3d9f02971d6f6c475c9e11c55b711b
Sampath Surineni <sampath.surineni@gmail.com>
2026-09-10 06:00:24 +0530

    Auto-sync from ssurineni@ssurine-ltm3c4c.internal.salesforce.com

--- src/components/TempPlayground/TempPlayground.jsx
+++ src/components/TempPlayground/TempPlayground.jsx
@@ -1,11 +1,15 @@
 import users from "./users"
 function TempPlayground() {
-  return <table>
+  const pageSizes = [5, 10, 15]
+  return <>
+  <select>
+    {pageSizes.map(aPageSize => <value>{aPageSize}</value>)}
+    </select><table>
     <thead><tr><th>Name</th><th>Age</th><th>Occupation</th></tr></thead>
     <tbody>
-      {users.map(aUser => <tr key={aUser.id}><td>{aUser.name}</td></tr>)}
+      {users.map(aUser => <tr key={aUser.id}><td>{aUser.name}</td><td>{aUser.age}</td><td>{aUser.occupation}</td></tr>)}
     </tbody>
-  </table>
+  </table></>
 }
 
 export default TempPlayground
