c080200ad8b1b3a508c74d0effab53e8d4baeff8
Sampath Surineni <sampath.surineni@gmail.com>
2026-09-10 08:00:29 +0530

    Auto-sync from ssurineni@ssurine-ltm3c4c.internal.salesforce.com

--- src/components/TempPlayground/TempPlayground.jsx
+++ src/components/TempPlayground/TempPlayground.jsx
@@ -3,6 +3,7 @@ import {useState} from 'react'
 function TempPlayground() {
   const pageSizes = [5, 10, 15]
   const [selectedPageSize, setSelectedPageSize] = useState(pageSizes[0])
+  const [cursorPos, setCursorPos] = useState(0)
   return <>
     <label>
       Rows :
@@ -11,10 +12,12 @@ function TempPlayground() {
           value={selectedPageSize}>{aPageSize}</option>)}
       </select>
       </label>
+      <button onClick={() => setCursorPos(cursorPos - selectedPageSize)}>prev</button>
+      <button onClick={() => setCursorPos(cursorPos + selectedPageSize)}>next</button>
     <table>
       <thead><tr><th>Name</th><th>Age</th><th>Occupation</th></tr></thead>
       <tbody>
-        {users.map(aUser => <tr key={aUser.id}><td>{aUser.name}</td><td>{aUser.age}</td><td>{aUser.occupation}</td></tr>)}
+        {users.slice(cursorPos, cursorPos+ selectedPageSize).map(aUser => <tr key={aUser.id}><td>{aUser.name}</td><td>{aUser.age}</td><td>{aUser.occupation}</td></tr>)}
       </tbody>
     </table></>
 }
