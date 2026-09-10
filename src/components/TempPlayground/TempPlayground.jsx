62251d221f819272ff14c177d2678bf4bd7ec68d
Sampath Surineni <sampath.surineni@gmail.com>
2026-09-10 07:50:25 +0530

    Auto-sync from ssurineni@ssurine-ltm3c4c.internal.salesforce.com

--- src/components/TempPlayground/TempPlayground.jsx
+++ src/components/TempPlayground/TempPlayground.jsx
@@ -1,16 +1,22 @@
 import users from "./users"
+import {useState} from 'react'
 function TempPlayground() {
   const pageSizes = [5, 10, 15]
+  const [selectedPageSize, setSelectedPageSize] = useState(pageSizes[0])
   return <>
-  <select>
-    {pageSizes.map(aPageSize => <option key={aPageSize}
-    value={aPageSize}>{aPageSize}</option>)}
-    </select><table>
-    <thead><tr><th>Name</th><th>Age</th><th>Occupation</th></tr></thead>
-    <tbody>
-      {users.map(aUser => <tr key={aUser.id}><td>{aUser.name}</td><td>{aUser.age}</td><td>{aUser.occupation}</td></tr>)}
-    </tbody>
-  </table></>
+    <label>
+      Rows :
+      <select onChange={(e) => setSelectedPageSize(e.target.value)}>
+        {pageSizes.map(aPageSize => <option key={aPageSize}
+          value={selectedPageSize}>{aPageSize}</option>)}
+      </select>
+      </label>
+    <table>
+      <thead><tr><th>Name</th><th>Age</th><th>Occupation</th></tr></thead>
+      <tbody>
+        {users.map(aUser => <tr key={aUser.id}><td>{aUser.name}</td><td>{aUser.age}</td><td>{aUser.occupation}</td></tr>)}
+      </tbody>
+    </table></>
 }
 
 export default TempPlayground
