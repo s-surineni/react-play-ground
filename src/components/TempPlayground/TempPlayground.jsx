4edef8221ddca81cdc6770aea9b595c77eda9ac2
Sampath Surineni <sampath.surineni@gmail.com>
2026-09-16 07:50:27 +0530

    Auto-sync from ssurineni@ssurine-ltm3c4c.internal.salesforce.com

--- src/components/TempPlayground/TempPlayground.jsx
+++ src/components/TempPlayground/TempPlayground.jsx
@@ -1,39 +1,6 @@
-import users from "./users"
-import {useState} from 'react'
-function TempPlayground() {
-  const pageSizes = [5, 10, 15]
-  const [userData, setUserData] = useState([...users]);
-  const [selectedPageSize, setSelectedPageSize] = useState(pageSizes[0])
-  const [cursorPos, setCursorPos] = useState(0)
-  const [sortAsc, setSortAsc] = useState(true)
-  function sortByName() {
-    const sortVal = 'name'
-    userData.sort((a, b) => {
-      const res = a[sortVal].localeCompare(b[sortVal])
-      return sortAsc? res : - res
-    })
-    setUserData([...userData])
-    setSortAsc(!sortAsc)
-  }
-  return <>
-    <label>
-      Rows :
-      <select onChange={(e) => {
-        setCursorPos(0)
-        setSelectedPageSize(Number(e.target.value))}}
-        value={selectedPageSize}>
-        {pageSizes.map(aPageSize => <option key={aPageSize}
-          value={aPageSize}>{aPageSize}</option>)}
-      </select>
-      </label>
-      <button onClick={() => setCursorPos(cursorPos - selectedPageSize)} disabled={cursorPos <= 0}>prev</button>
-      <button onClick={() => setCursorPos(cursorPos + selectedPageSize)} disabled={cursorPos + selectedPageSize >= users.length}>next</button>
-    <table>
-      <thead><tr><th onClick={sortByName}>Name</th><th>Age</th><th>Occupation</th></tr></thead>
-      <tbody>
-        {userData.slice(cursorPos, cursorPos+ selectedPageSize).map(aUser => <tr key={aUser.id}><td>{aUser.name}</td><td>{aUser.age}</td><td>{aUser.occupation}</td></tr>)}
-      </tbody>
-    </table></>
+import TaskInput from './TaskInput'
+function TempPlayground(){
+  return <TaskInput />;
 }
 
 export default TempPlayground
